62ba193fe69bcbdd15859c170afefd8b6ea1da1b
Your Name <gmoaokgsliem@gmail.com>
2019-06-08 23:43:43 +0300

    Initial Commit

---
+++ Todosha/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todosha
+//
+//  Created by Максим Белугин on 08/06/2019.
+//  Copyright © 2019 Максим Белугин. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
